00687b078266c339b94e420f19b4e66d6580b85d
MaxHuw <m.huw.graham@gmail.com>
2019-02-25 23:32:10 +0000

    Some rough daft of the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ActiveRecord::Base
 
   has_many :review
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email, password)
+    user = where('LOWER(email) = ?', email.downcase.strip)[0]
+    user && user.authenticate(password)
+  end
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @user = User.new
+  end
+
+  it 'saves with all fields filled in' do
+    full_user = User.new(first_name: 'a', last_name: 'b', email: 'email@email.com', password: 'apples')
+    full_user.save
+    expect(full_user).to be_valid
+  end
+
+  it 'is invalid without a first_name' do
+    expect(@user).to_not be_valid
+    expect(@user.errors.messages[:first_name]).to include('can\'t be blank')
+  end
+  
+  it 'is invalid without a last_name' do
+    expect(@user).to_not be_valid
+    expect(@user.errors.messages[:last_name]).to include('can\'t be blank')
+  end
+
+  it 'is invalid without password' do
+    expect(@user).to_not be_valid
+    expect(@user.errors.messages[:password]).to include('can\'t be blank')
+  end
+
+  it 'is invalid without an email' do
+    expect(@user).to_not be_valid
+    expect(@user.errors.messages[:email]).to include('can\'t be blank')
+  end
+
 end
